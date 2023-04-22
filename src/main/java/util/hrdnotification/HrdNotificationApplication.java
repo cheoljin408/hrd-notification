81b2e49abcd1300898b0fce588c64336568cb111
cheoljin408 <kcj409@naver.com>
2023-04-22 13:39:03 +0900

    Fix: application.pid 설정

--- src/main/java/util/hrdnotification/HrdNotificationApplication.java
+++ src/main/java/util/hrdnotification/HrdNotificationApplication.java
@@ -2,6 +2,7 @@ package util.hrdnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -9,7 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HrdNotificationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HrdNotificationApplication.class, args);
+        SpringApplication application = new SpringApplication(HrdNotificationApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
